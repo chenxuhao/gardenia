24150e03d01b70a54f7f6c125b9df91b223863cc
Mengjia Yan <myan8@illinois.edu>
2017-01-01 08:13:33 -0600

    fix all Makefiles

--- src/common.mk
+++ src/common.mk
@@ -1,5 +1,7 @@
 CUDA_HOME=/usr/local/cuda
 GARDINIA_ROOT=/home/cxh/gardinia
+CUB_DIR=/home/cxh/cub-1.1.1
+B40_DIR=~/back40computing-read-only
 GCC=gcc
 GXX=g++
 NVCC=$(CUDA_HOME)/bin/nvcc
@@ -9,4 +11,6 @@ PAR_FLAG = -fopenmp
 #NVFLAGS=-g -arch=$(COMPUTECAPABILITY) #-Xptxas -v
 NVFLAGS=-w -O3 -arch=$(COMPUTECAPABILITY) #-Xptxas -v
 INCLUDES=-I$(CUDA_HOME)/include -I$(GARDINIA_ROOT)/include
+LIBS=-L$(CUDA_HOME)/lib64
+#EXTRA=-cudart shared
 BIN=$(GARDINIA_ROOT)/bin/
